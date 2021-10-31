0866af432613b11a9037e62fc739dd14601395e9
Huong Nguyen <huongnguyen10102@gmail.com>
2021-10-31 20:00:08 +0200

    1st commit, add row components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+
+function App() {
+  return (
+    <div className="App">
+      <Row title="NETFLIX ORIGINAL"/>
+      <Row title="Trending Now"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Row() {
+    return (
+        <div>
+            {/* title */}
+
+            {/* container -> poster */}
+        </div>
+    );
+}
+
+export default Row
