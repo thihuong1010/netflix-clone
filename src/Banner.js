683ef42bfad074b50fcebe934a44d14f45b84e95
Huong Nguyen <huongnguyen10102@gmail.com>
2021-11-01 23:42:50 +0200

    fetch all categories

---
+++ src/Banner.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect (() => {
+        async function fetchData() {
+            
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <header> {/* background img */}
+            {/* title */}
+            {/* div > 2 buttons */}
+            {/* description */}
+        </header>
+    )
+}
+
+export default Banner
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from "./axios"; 
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     // a snippet of code which runs based on a specific condition/variable
@@ -25,8 +26,9 @@ function Row({title, fetchUrl}) {
             <div className="row__posters">
                 {/* several row__posters */}
                 {movies.map(movie => (
-                    <img className="row__poster" 
-                    src={`${base_url}${movie.poster_path}`} 
+                    <img key={movie.id}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} 
                     alt={movie.name}/>  
                 ))}
             </div>
