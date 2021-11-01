a3b14ea6cff14a53e00d417483b7186048dab62e
Huong Nguyen <huongnguyen10102@gmail.com>
2021-11-02 00:16:34 +0200

    add banner without CSS

--- src/Banner.js
+++ src/Banner.js
@@ -1,20 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import requests from './requests';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
 
     useEffect (() => {
         async function fetchData() {
-            
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            // array of movies go here. but we just need one
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
         }
         fetchData();
     }, []);
 
+    console.log(movie);
+
     return (
-        <header> {/* background img */}
-            {/* title */}
-            {/* div > 2 buttons */}
-            {/* description */}
+        <header className="banner"
+        style = {{
+            backgroundSize: "cover",
+            backgroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+            )`,
+            backgroundPosition: "center center",
+        }}
+        > 
+            <div className="banner__contents">
+                {/* background img */}
+                {/* title */}
+                <h1>
+                    {movie?.name || movie?.title || movie?.original_name}
+                </h1>
+                {/* div > 2 buttons */}
+                {/* description */}
+            </div>
         </header>
     )
 }
